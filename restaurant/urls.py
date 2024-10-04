88c3782bd75bd04ace49cebade6da2ae295fbf3c
SmitaMohanty2 <smita.bbsr@gmail.com>
2024-10-04 19:58:02 +0200

    Updated with static content added

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import sayHello
+from .views import sayHello, index
+from . import views
 
 urlpatterns = [
-    path('', sayHello, name='sayHello'),
+    path('sayhello', sayHello, name='sayHello'),
+    path('', index, name='index')
 ]
